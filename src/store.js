88dee3ce452e043f4e1d7829c74523e6c43dc8b8
jsfanatik <jsfanatik@gmail.com>
2019-08-31 00:37:15 -0400

    first commit

---
+++ src/store.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { db } from '@/main'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    events: []
+  },
+  mutations: {
+    setEvents: (state, events) => {
+      state.events = events
+    }
+  },
+  getters: {
+    getEvents: state => {
+      return state.events
+    }
+  },
+  actions: {
+    setEvents: async context => {
+      let snapshot = await db.collection('calEvent').get()
+      const events = []
+      snapshot.forEach(doc => {
+        let appData = doc.data()
+        appData.id = doc.id
+        events.push(appData)
+      })
+      context.commit('setEvents', events)
+    }
+  }
+})
